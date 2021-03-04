d621fa88285a620328db370eb66d4000f0e11023
Andrew Sikora <andrew.skr4@gmail.com>
2021-03-04 13:14:23 -0500

    added filter for Buy upgrade type

--- marketwatch_scraper/v2/scraper_v2.py
+++ marketwatch_scraper/v2/scraper_v2.py
@@ -11,6 +11,8 @@ page = urlopen(url).read()
 soup = BeautifulSoup(page, features="lxml")
 upgrades = []
 old_prices = []
+firms = []
+firms_ratings = []
 pairs = []
 sample = []
 nums = []
@@ -30,7 +32,19 @@ if (len(sys.argv)==1):
         #print(tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text)
         if(tds[3].text == 'Upgrades'):
             upgrades.append(tds[1].text)
+            firms.append(tds[4].text)
             #print(tds[1].text, tds[3].text)
+            rating_url = 'https://www.benzinga.com/stock/'+str(tds[1].text)+'/ratings'
+            rating_page = urlopen(rating_url).read()
+            rating_soup = BeautifulSoup(rating_page, features="lxml")
+            print(rating_soup)
+            for ratetr in rating_soup.find_all('tr')[1:]:
+                #print(tr)
+                ratetds = ratetr.find_all('td')
+                #print(tds)
+                #if(tds[1].text == str(firms[i])):
+                firms_ratings.append(str(ratetds[3].text))
+                break
 else:
     f = open(sys.argv[1], 'r')
     #f = open(d+".txt", 'r')
@@ -38,8 +52,10 @@ else:
     for line in lines:
         upgrades.append(line.split()[0])
         old_prices.append(line.split()[1])
-        pair = (line.split()[0], line.split()[1])
-        pairs.append(pair)
+        #print(line.split()[-1])
+        if(line.split()[-1] =='Buy'):
+            pair = (line.split()[0], line.split()[1], line.split()[-1])
+            pairs.append(pair)
     if (len(sys.argv)==3):
         shuffle(pairs)
         temp = int(sys.argv[2])
@@ -47,20 +63,36 @@ else:
         print(sample)
 print(upgrades)
 
+if(len(sys.argv)==2):
+    print("Ticker  Price_i  Price_n  % increase")
+
 if (len(sys.argv) <3):
     for i in range(len(upgrades)):
+        firm_rating = "null"
         url = 'https://www.marketwatch.com/investing/stock/'+str(upgrades[i])
+        rating_url = 'https://www.benzinga.com/stock/'+str(upgrades[i])+'/ratings'
+        rating_page = urlopen(rating_url).read()
+        rating_soup = BeautifulSoup(rating_page, features="lxml")
+        for tr in rating_soup.find_all('tr')[1:]:
+            #print(tr)
+            tds = tr.find_all('td')
+            #print(tds)
+            #if(tds[1].text == str(firms[i])):
+            firm_rating = str(tds[3].text)
+            break
+        #print(firm_rating)
         page = urlopen(url).read()
         soup = BeautifulSoup(page, features="lxml")
         change = soup.find(lambda tag: tag.name == 'h3' and tag.get('class') == ['intraday__price'])
         price = (change.text).split("\n")[2]
         #print(price)
         if (len(sys.argv)==1):
-            g.write(upgrades[i] + " " +price+"\n")
+            g.write(upgrades[i] + " " +price+" "+firms[i]+" "+firms_ratings[i]+"\n")
             #print(upgrades[i], change.text)
         if (len(sys.argv)==2):
-            print(str(upgrades[i])+" "+str(price)+" "+ str(old_prices[i]) + " " + str(100*(float(price)-float(old_prices[i]))/float(old_prices[i]))+"%")
-            num = 100*(float(price)-float(old_prices[i]))/float(old_prices[i])
+            print(str(upgrades[i])+" "+ str(old_prices[i]) + " "+str(price)+" " + str(100*(float(price)-float(old_prices[i]))/float(old_prices[i]))+"% "+firm_rating)
+            #get increase as fraction
+            num = (float(price)-float(old_prices[i]))/float(old_prices[i])
             total_change += num
             opening_cost += float(old_prices[i])
             current_value += float(price)
@@ -70,10 +102,11 @@ if (len(sys.argv) <3):
         #total_change += float(num)
     #print(total)
     #print("Percent change: "+str(100*(current_value-opening_cost)/opening_cost))
-    print(total_change)
+    #print the total fractional increase multiplied by how much each stock is roughly worth
+    print(total_change*100)
 
 else: 
-    for ticker, old_price in sample:
+    for ticker, old_price, rating in sample:
         url = 'https://www.marketwatch.com/investing/stock/'+str(ticker)
         page = urlopen(url).read()
         soup = BeautifulSoup(page, features="lxml")
